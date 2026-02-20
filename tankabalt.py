3b4f15311179d3b0f7b1ea2eaf1c1faf775cdf4a
rehan <rehan.akanda@gmail.com>
2026-02-20 12:13:09 -0500

    initial file

---
+++ tankabalt.py
@@ -0,0 +1,29 @@
+import sys
+
+import pygame
+import pygame.locals
+
+
+WIDTH, HEIGHT = 500, 500
+
+
+def main():
+    fps = 60
+    fps_clock = pygame.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    while True:
+        screen.fill("#000000")
+
+        for event in pygame.event.get():
+            if event.type == pygame.locals.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.flip()
+        fps_clock.tick(fps)
+
+
+if __name__ == "__main__":
+    main()
